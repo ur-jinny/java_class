11654ed0d5cb8d23ccd4866862ed95cdeae361c1
ur-jinny <youjin45089@gamil.com>
2023-12-06 13:21:13 +0900

    While 예제

--- src/ch07_while/Ex04_WhileEx.java
+++ src/ch07_while/Ex04_WhileEx.java
@@ -5,11 +5,18 @@ public class Ex04_WhileEx {
 
     public static void main(String[] args) {
         /**
-         * random() 메서드를 이용해서 1~6dl 나오는 주사위를 설정하고
+         * random() 메서드를 이용해서 1~6이 나오는 주사위를 설정하고
          * 주사위가 계속 던져지면서 6이 나오면 종료되는 코드 작성
          */
+    boolean dice = true;
+        while(dice){
+            int i = ((int)(Math.random() * 6)+1);
+            System.out.println(i);
+            if(i == 6) {
+                dice = false;
+            }
 
-        System.out.println((int)(Math.random() * 6)+1);
+        }
 
 
     }
---
+++ src/ch07_while/Ex05_WhileEx.java
@@ -0,0 +1,58 @@
+package ch07_while;
+
+import java.util.Scanner;
+
+public class Ex05_WhileEx {
+    public static void main(String[] args) {
+        /**
+         * random() 메서드를 이용해서 1~100 사이의 숫자를 하나 만들고
+         * 반복문 안에서 해당 숫자를 맞출때까지 계속 숫자를 입력받도록 하고
+         * 숫자를 맞추면 종료하고 몇 번만에 맞췄는지를 출력해줌
+         */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("숫자");
+        int num1 = scanner.nextInt();
+        int i = ((int)(Math.random() * 100)+1);
+        boolean run = true;
+
+
+        while (run){
+            if(num1 != i) {
+                System.out.println("땡");
+                num1 = scanner.nextInt();
+
+            }else{
+                System.out.println("정답");
+                run = false;
+            }
+        }
+        //선생님 풀이 위에랑 똑같음(다른공식)
+        // Scanner scanner = new Scanner(System.in);
+        //boolean run = true;
+        int input = 0, count = 0;
+        System.out.println("up & down");
+        int answer = (int) (Math.random() * 100) + 1;
+        while (run) {
+            int count2 = 0;
+            System.out.print("숫자를 입력하세요: ");
+            input = scanner.nextInt();
+            count++;
+            count2++;
+            System.out.println("answer = " + answer);
+            if (input > answer) {
+                System.out.println("더 작은 수를 입력하세요!");
+            } else if (input < answer) {
+                System.out.println("더 큰 수를 입력하세요!");
+            } else {
+                System.out.println("맞았습니다!");
+                System.out.println("시도 횟수는 " + count + "회 입니다.");
+                System.out.println("시도 횟수는 " + count2 + "회 입니다.");
+            }
+        }
+
+    }
+}
+
+
+
+
