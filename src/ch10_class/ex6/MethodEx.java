37dab9db4defbbfe94100fef1a6dd5f8b21f3ea2
ur-jinny <youjin45089@gamil.com>
2023-12-12 18:57:24 +0900

    객체지향 시작 -method

--- src/ch10_class/ex6/MethodEx.java
+++ src/ch10_class/ex6/MethodEx.java
@@ -1,4 +1,44 @@
 package ch10_class.ex6;
 
 public class MethodEx {
+    // 매개변수 x, 리턴 x
+    public void method1() {
+        System.out.println("MethodEx.method1");
+    }
+
+    // 매개변수 o, 리턴 x
+    public void method2(String param1) {
+        System.out.println("param1 = " + param1);
+        String str2 = "집에 가고 싶다";
+        System.out.println(param1 + str2);
+    }
+
+    // 매개변수 x, 리턴 o
+    // 리턴 String
+    public String method3() {
+        System.out.println("MethodEx.method3");
+//        return "부르셨나요?";
+        String returnValue = "부르셨나요?";
+        return returnValue;
+    }
+
+    // 리턴 int
+    public int method4() {
+        int num1 = 10;
+        int num2 = 20;
+        return num1 + num2;
+    }
+
+    // 매개변수 o, 리턴 o
+    public String method5(String param1, int param2) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+        String result = "전달받은 값은 " + param1 + "과" + param2 + "입니다.";
+        return result;
+    }
+
+    public int method6(int num1, int num2) {
+        int result = num1 + num2;
+        return result;
+    }
 }
+
--- src/ch10_class/ex6/MethodExMain.java
+++ src/ch10_class/ex6/MethodExMain.java
@@ -1,4 +1,32 @@
 package ch10_class.ex6;
 
 public class MethodExMain {
+    public static void main(String[] args) {
+        MethodEx methodEx = new MethodEx();
+        System.out.println("호출 전");
+        // method1 호출
+        methodEx.method1();
+        System.out.println("호출 후");
+        // method2 호출
+        methodEx.method2("안녕하세요");
+        String str1 = "오늘은 화요일";
+        methodEx.method2(str1);
+
+        // method3 호출
+        String result = methodEx.method3();
+        System.out.println("result = " + result);
+        System.out.println(methodEx.method3());
+
+        // method4 호출 후 결과값 출력
+        int result1 = methodEx.method4();
+        System.out.println("result1 = " + result1);
+
+        // method5 호출
+        String result2 = methodEx.method5("안녕", 1000);
+        System.out.println("result2 = " + result2);
+
+        int result3 = methodEx.method6(10, 20);
+        System.out.println("result3 = " + result3);
+
+    }
 }
