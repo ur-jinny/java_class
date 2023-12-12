fc6349d29519c511ec2f579477318d5b010808a7
ur-jinny <youjin45089@gamil.com>
2023-12-12 19:03:57 +0900

    객체지향 시작 -계산기

--- src/ch10_class/ex7/Calculator.java
+++ src/ch10_class/ex7/Calculator.java
@@ -1,4 +1,72 @@
 package ch10_class.ex7;
 
+import java.util.Scanner;
 public class Calculator {
+    Scanner scanner = new Scanner(System.in);
+    /**
+     * 덧셈 메서드
+     * method name: sum
+     * parameter: int 타입 2개
+     * return: x
+     * 실행내용
+     *  - 매개변수로 전달 받은 2개 값의 합을 출력
+     */
+    /**
+     *
+     * @param num1
+     * @param num2
+     */
+    public void sum(int num1, int num2) {
+        System.out.println("합계: " + (num1 + num2));
+    }
+
+    /**
+     * 뺄셈 메서드
+     * method name: sub
+     * parameter: int 타입 2개
+     * return: x
+     * 실행내용
+     *  - 매개변수로 전달 받은 2개 값의 차를 출력
+     */
+    public void sub(int num1, int num2) {
+        int result = num1 - num2;
+        System.out.println("result = " + result);
+    }
+
+    /**
+     * 곱셈 메서드
+     * method name: mul
+     * parameter: int 타입 2개
+     * return: int 타입
+     * 실행내용
+     *  - 매개변수로 전달 받은 2개 값의 곱 결과를 리턴
+     */
+    /**
+     *
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public int mul(int num1, int num2) {
+        int result = num1 * num2;
+        return result;
+    }
+
+    /**
+     * 나눗셈 메서드
+     * method name: div
+     * parameter: 없음
+     * return: int 타입
+     * 실행내용
+     *  - 스캐너로 나눗셈에 필요한 정수 2개를 입력받음
+     *  - 나눗셈 결과를 리턴
+     */
+    public int div() {
+        System.out.print("첫번째 숫자: ");
+        int num1 = scanner.nextInt();
+        System.out.print("두번째 숫자: ");
+        int num2 = scanner.nextInt();
+        int result = num1 / num2;
+        return result;
+    }
 }
--- src/ch10_class/ex7/CalculatorMain.java
+++ src/ch10_class/ex7/CalculatorMain.java
@@ -1,4 +1,52 @@
 package ch10_class.ex7;
 
+import java.util.Scanner;
 public class CalculatorMain {
+    public static void main(String[] args) {
+        /**
+         * 실행하면 반복문으로 메뉴를 출력
+         * 1.덧셈 2.뺄셈 3.곱셈 4.나눗셈
+         * 각 번호를 선택하면 Calculator 클래스에 있는 해당 메서드를 호출하여 결과를 출력함
+         */
+
+        Scanner scanner = new Scanner(System.in);
+        boolean run = true;
+        int selectNo = 0;
+        int num1 = 0, num2 = 0;
+        Calculator calculator = new Calculator();
+
+        while (run) {
+            System.out.println("--------------------------------------------------------------");
+            System.out.println("1.덧셈 | 2.뺄셈 | 3.곱셈 | 4.나눗셈 | 5.종료");
+            System.out.println("--------------------------------------------------------------");
+            System.out.print("선택> ");
+            selectNo = scanner.nextInt();
+            if (selectNo == 1) {
+                System.out.print("첫번째 숫자: ");
+                num1 = scanner.nextInt();
+                System.out.print("두번째 숫자: ");
+                num2 = scanner.nextInt();
+                calculator.sum(num1, num2);
+            } else if (selectNo == 2) {
+                System.out.print("첫번째 숫자: ");
+                num1 = scanner.nextInt();
+                System.out.print("두번째 숫자: ");
+                num2 = scanner.nextInt();
+                calculator.sub(num1, num2);
+            } else if (selectNo == 3) {
+                System.out.print("첫번째 숫자: ");
+                num1 = scanner.nextInt();
+                System.out.print("두번째 숫자: ");
+                num2 = scanner.nextInt();
+                int result = calculator.mul(num1, num2);
+                System.out.println("result = " + result);
+            } else if (selectNo == 4) {
+                int result = calculator.div();
+                System.out.println("result = " + result);
+            } else if (selectNo == 5) {
+                run = false;
+            }
+        }
+    }
 }
+
