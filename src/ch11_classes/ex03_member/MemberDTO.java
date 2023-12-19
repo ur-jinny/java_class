3cece5b865ecd23bcbce43199387e9410c68bbea
ur-jinny <youjin4508@gmail.com>
2023-12-19 15:17:14 +0900

    회원 프로젝트 - 기본 구성 완료

---
+++ src/ch11_classes/ex03_member/MemberDTO.java
@@ -0,0 +1,74 @@
+package ch11_classes.ex03_member;
+
+public class MemberDTO {
+    private Long id;
+    private String memberEmail;
+    private String memberPassword;
+    private String memberName;
+    private String memberMobile;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMemberEmail() {
+        return memberEmail;
+    }
+
+    public void setMemberEmail(String memberEmail) {
+        this.memberEmail = memberEmail;
+    }
+
+    public String getMemberPassword() {
+        return memberPassword;
+    }
+
+    public void setMemberPassword(String memberPassword) {
+        this.memberPassword = memberPassword;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public String getMemberMobile() {
+        return memberMobile;
+    }
+
+    public void setMemberMobile(String memberMobile) {
+        this.memberMobile = memberMobile;
+    }
+
+    public MemberDTO() {
+
+    }
+
+    private static Long idValue = 1L;
+
+    public MemberDTO(String memberEmail, String memberPassword, String memberName, String memberMobile) {
+        this.id = idValue++;
+        this.memberEmail = memberEmail;
+        this.memberPassword = memberPassword;
+        this.memberName = memberName;
+        this.memberMobile = memberMobile;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberDTO{" +
+                "id=" + id +
+                ", memberEmail='" + memberEmail + '\'' +
+                ", memberPassword='" + memberPassword + '\'' +
+                ", memberName='" + memberName + '\'' +
+                ", memberMobile='" + memberMobile + '\'' +
+                '}';
+    }
+}
